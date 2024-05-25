dd81ce61868763a32871b63efed3110d8857d654
MarkosComK <markoscomks@gmail.com>
2024-05-25 22:59:05 +0100

    created contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+
+export default function contact() {
+  return (
+    <main>
+      <div>work page</div>
+      <Link href="/work">about page</Link>
+      <Link href="/misc">contact page</Link>
+    </main>
+  );
+}
