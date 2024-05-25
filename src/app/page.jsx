0ab3b696699d44040e52938d0f27aee8fcffc5ee
MarkosComK <markoscomks@gmail.com>
2024-05-26 00:26:46 +0100

    simple setup for page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,21 @@
 import Link from "next/link"
+import styles from "./home.module.css";
 
 export default function home() {
   return (
-    <main>
-      <div>home page</div>
-      <Link href="/misc">mist page</Link>
-      <Link href="/work">work here</Link>
+    <main className={styles.main}>
+      <nav className={styles.nav}>
+        <div>Dir / dir</div>
+        <ul>
+          <li>
+            <Link href="/misc">mist page</Link>
+          </li>
+          <li>
+            <Link href="/work">work here</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>home page</h1>
     </main>
   );
 }
