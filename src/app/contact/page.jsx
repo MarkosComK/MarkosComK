2bcc3243ded54a7000c3d2d99677d6c8fb148078
MarkosComK <markoscomks@gmail.com>
2024-05-25 23:09:55 +0100

    renamed page div

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function contact() {
   return (
     <main>
-      <div>work page</div>
+      <div>contact page</div>
       <Link href="/work">about page</Link>
       <Link href="/misc">contact page</Link>
     </main>
