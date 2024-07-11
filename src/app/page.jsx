d08b431bec22608c8ff2f350fb9235c8eaef58cd
markoscomk <markoscomks@gmail.com>
2024-07-11 23:33:01 +0100

    Changed hover color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,8 +18,8 @@ export default function home() {
     <div className="h-full w-1/2">
       <div className={`${screen ? "open": "close"} bg-charcoal custom-hover border-b border-green`} onClick={() => handleScreen()}>
         <span className={`${screen ? "-mt-20" : ""} absolute transition-all duration-500 ease-in-out`}>
-          <h1 className="p-3">Markos Santos</h1>
-          <p className="pl-3">Developer</p>
+          <h1 className="p-3 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]" >Markos Santos</h1>
+          <p className="pl-3 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">Creative Developer</p>
         </span>
         <div className="flex w-screen h-12 items-center justify-end">
           <Image className={`${screen ? "m-4" : "-mt-24" } transition-all duration-500 delay-500 z-50`} src={"/camera-cam.gif"} width={50} height={50}/>
@@ -33,11 +33,11 @@ export default function home() {
         <div>
           <div className="lines z-30"></div>
           <div className="noise"></div>
-          <p className="relative z-50">
-            From tinkering with tech to building beautiful interfaces, I'm a passionate front-end developer fueled by creativity.  A few years ago, my coding journey began with exploring the inner workings of computers.  But it was web development that truly sparked my passion.  It allows me to combine my artistic sensibilities (honed through digital art and design tools like Photoshop, Illustrator, and Figma) with my programming skills to craft user experiences that are both beautiful and intuitive.
+          <p className="relative z-50 shadow [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
+            From tinkering with tech to building beautiful interfaces, I'm <span className="glitch" data-text="Markos">Markos</span> a passionate front-end developer fueled by creativity.  A few years ago, my coding journey began with exploring the inner workings of computers.  But it was web development that truly sparked my passion.  It allows me to combine my artistic sensibilities (honed through digital art and design tools like Photoshop, Illustrator, and Figma) with my programming skills to craft user experiences that are both beautiful and intuitive.
           </p>
           <br />
-          <p className="relative z-50">
+          <p className="relative z-50 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
             Front-end development feels like the perfect intersection where my talents can truly shine. It allows me to continuously learn and refine my skills, pushing me towards becoming a well-rounded developer.  If you're looking for a creative and enthusiastic front-end developer to bring your project to life, let's connect!
           </p>
         </div>
