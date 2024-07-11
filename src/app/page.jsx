1bfd511262399ba65faaa4e6d1f82fb0d3041fe6
markoscomk <markoscomks@gmail.com>
2024-07-11 10:34:56 +0100

    Added base styles using tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,25 @@ import Link from "next/link"
 
 export default function home() {
   return (
-    <main>
-      <div>home page</div>
-      <Link href="/misc">mist page</Link>
-      <Link href="/work">work here</Link>
-    </main>
+    <section className="flex h-screen border border-green">
+    <div className="h-full w-1/2">
+      <div className="h-1/2 hover:bg-orange border-b border-green">
+        <span>
+          <h1 className="p-3">Markos Santos</h1>
+        </span>
+      </div>
+      <p className="h-1/2 hover:bg-orange">Developer</p>
+    </div>
+    <div className="flex flex-col w-1/2 h-full border-l border-green">
+      <div className="flex w-full h-1/2 border-b border-green">
+        <div className="w-1/2 hover:bg-orange border-r border-green">teste3</div>
+        <div className="w-1/2 hover:bg-orange">teste4</div>
+      </div>
+      <div className="flex w-full h-1/2">
+        <div className="w-1/2 hover:bg-orange border-r border-green">teste5</div>
+        <div className="w-1/2 hover:bg-orange">teste6</div>
+      </div>
+    </div>
+    </section>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'charcoal': '#014040',
+      'green': '#02735E',
+      'jade': '#03A678',
+      'orange': '#F27405',
+      'brown': '#731702',
+    },
+  },
+  plugins: [],
+}
