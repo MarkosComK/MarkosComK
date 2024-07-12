10171e624ab0363b7a8d2b3ceb2f4265c5b8c886
markoscomk <markoscomks@gmail.com>
2024-07-12 15:17:03 +0100

    Fixed bug with contact button

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,7 @@
 export default function ContactButton({ screen }) {
   return (
     <button
-    className={`${screen ? "" : "-bottom-6 -right-6" } transition-all duration-500 delay-1000 absolute z-50 bottom-6 right-6`}>
+    className={`${screen ? "bottom-6 right-6" : "-bottom-6 -right-6" } transition-all duration-500 delay-1000 absolute z-50 `}>
       contact
     </button>
   );
