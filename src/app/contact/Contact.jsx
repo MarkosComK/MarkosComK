e44ee4e20b9e8cd910c748f58ca9a76dec272c26
markoscomk <markoscomks@gmail.com>
2024-07-12 20:48:26 +0100

    Fixed bug with animations in screens in-out

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -2,9 +2,40 @@ import Noise from "../effects/noise";
 
 export default function Contact({ display, setContactDisplay }) {
   return (
-    <div className={`${display ? "open bottom-0" : "close custom-hover"} transition-all duration-500 bg-charcoal`} onClick={() => setContactDisplay(!display)} id="contact">
+    <div className={`${display ? "open" : "close custom-hover"} transition-all duration-500 bottom-0 bg-charcoal`} onClick={() => setContactDisplay(!display)} id="contact">
     {display ? <Noise /> : ""}
         <h2 className={`p-4`}>Contact</h2>
+        <div className={`${display ? "" : "hidden" } flex flex-col h-[90%] justify-around p-4`}>
+            <a
+                className={`
+                ${display ? "flex" : ""}
+                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
+                transition-all hover:bg-jade hover:text-black`}
+                href="mailto:markoscomks@gmail.com"
+                >
+                <p>
+                    markoscomks@gmail.com
+                </p>
+            </a>
+            <a
+                className={`
+                ${display ? "flex" : ""}
+                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
+                transition-all hover:bg-jade hover:text-black`}
+                
+                >
+                github.com/MarkosComK
+            </a>
+            <a
+                className={`
+                ${display ? "flex" : ""}
+                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
+                transition-all hover:bg-jade hover:text-black`}
+                
+                >
+                linkedin
+            </a>
+        </div>
     </div>
   )
 }
