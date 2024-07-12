a234394aaabab0ec2266de0a4657b8832d85285c
markoscomk <markoscomks@gmail.com>
2024-07-12 18:51:17 +0100

    finished contact button

--- src/app/contact/ContactButton.jsx
+++ src/app/contact/ContactButton.jsx
@@ -2,9 +2,13 @@
 
 export default function ContactButton({ display, setContactDisplay }) {
   return (
-    <button
-    className={`${display ? "bottom-6 right-6" : "-bottom-6 -right-6" } transition-all duration-500 delay-1000 absolute z-50 `} onClick={() => setContactDisplay(true)}>
+    <div
+    className={`${display ? "bottom-6 right-6" : "-bottom-12 -right-12" }
+    transition-all duration-500 delay-1000 absolute z-50 border
+    `} onClick={() => setContactDisplay(true)}>
+    <button className="w-full h-full hover:bg-jade hover:text-black p-2 transition-all duration-300">
       contact
     </button>
+    </div>
   );
 }
