571d1b4eb41bc6d0667ec448eb2fb25c02d82889
markoscomk <markoscomks@gmail.com>
2024-07-13 15:05:02 +0100

    Restarted proj

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,27 +23,10 @@ export default function home() {
   }
 
   return (
-    <div>
+    <div className="overflow-hidden relative">
     <Noise index={30}/>
-    <section className="flex h-screen border border-green" onMouseMove={handleMouse}>
-    <Cursor mousePos={mousePos}/>
-    <div className="h-full w-1/2">
-    <About display={aboutDisplay} setAboutDisplay={setAboutDisplay}/>
-    <Contact display={contactDisplay} setContactDisplay={setContactDisplay}/>
-    <LeftCorner display={aboutDisplay}/>
-    <ContactButton display={aboutDisplay} setContactDisplay={contactButton}/>
-    </div>
-    <div className="flex flex-col w-1/2 h-full border-l border-green">
-      <div className="flex w-full h-1/2 border-b border-green">
-        <div className="w-1/2 custom-hover border-r border-green">teste3</div>
-        <div className="w-1/2 custom-hover">teste4</div>
-      </div>
-      <div className="flex w-full h-1/2">
-        <div className="w-1/2 custom-hover border-r border-green">teste5</div>
-        <div className="w-1/2 custom-hover">teste6</div>
-      </div>
-    </div>
-    </section>
+      <section className="border border-jade" onMouseMove={handleMouse}>
+      </section>
     </div>
   );
 }
