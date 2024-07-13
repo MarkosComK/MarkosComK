ae4c86bab2e856a45ab19f8488e6d5c11599d3c9
markoscomk <markoscomks@gmail.com>
2024-07-13 15:34:19 +0100

    removed cursor for small screens

--- src/app/effects/cursor.jsx
+++ src/app/effects/cursor.jsx
@@ -1,7 +1,7 @@
 export default function Cursor({ mousePos }) {
   return (
     <div 
-    className={`h-6 w-4 absolute z-[100] bg-jade translate-x-3.5 animate-pulse`} 
+    className={`hidden h-6 w-4 lg:absolute z-[100] bg-jade translate-x-3.5 animate-pulse`} 
     style={{left: mousePos.x, top: mousePos.y}}>
     </div>
   )
