ce94689d5093bb363f3b6c7479adedfb71ef0710
markoscomk <markoscomks@gmail.com>
2024-07-13 16:57:33 +0100

    Added noise to contact page

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -3,6 +3,7 @@ import Noise from "../effects/noise";
 export default function Contact({ display, setContactDisplay }) {
   return (
     <div className={`${display ? "open border-t border-r border-b" : "close custom-hover border-b"} transition-all duration-500 bottom-0 bg-charcoal`} onClick={() => setContactDisplay(!display)} id="contact">
+    <Noise />
         <h2 className={`p-4`}>Contact</h2>
         <div className={`${display ? "" : "hidden" } flex flex-col h-[90%] justify-around p-4`}>
             <a
