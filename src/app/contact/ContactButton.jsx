40019c676b66703deb1abb293f8185f00849974d
markoscomk <markoscomks@gmail.com>
2024-07-13 19:04:57 +0100

    delay changes

--- src/app/contact/ContactButton.jsx
+++ src/app/contact/ContactButton.jsx
@@ -4,7 +4,7 @@ export default function ContactButton({ display, setContactDisplay }) {
   return (
     <div
     className={`${display ? "bottom-6 right-6" : "-bottom-12 -right-12" }
-    transition-all duration-500 delay-1000 absolute z-50 border
+    transition-all duration-500 delay-500 absolute z-50 border
     `} onClick={() => setContactDisplay(true)}>
     <button className="w-full h-full hover:bg-jade hover:text-black p-2 transition-all duration-300">
       contact
