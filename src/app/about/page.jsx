cfe5b0016ec14ea42e51f1cf403a9c9530940ca1
markoscomk <markoscomks@gmail.com>
2024-07-14 13:19:45 +0100

    Created new contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -53,6 +53,7 @@ export default function About({ display, setAboutDisplay}) {
           </p>
         </div>
         </div>
+        
       </div>
   );
 }
--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -2,40 +2,28 @@ import Noise from "../effects/noise";
 
 export default function Contact({ display, setContactDisplay }) {
   return (
-    <div className={`${display ? "open border-t border-r border-b" : "close custom-hover border-b"} transition-all duration-500 bottom-0 bg-charcoal`} onClick={() => setContactDisplay(!display)} id="contact">
-    <Noise />
-        <h2 className={`p-4`}>Contact</h2>
-        <div className={`${display ? "" : "hidden" } flex flex-col h-[90%] justify-around p-4`}>
-            <a
-                className={`
-                ${display ? "flex" : ""}
-                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
-                transition-all hover:bg-jade hover:text-black`}
-                href="mailto:markoscomks@gmail.com"
-                >
-                <p>
-                    markoscomks@gmail.com
-                </p>
-            </a>
-            <a
-                className={`
-                ${display ? "flex" : ""}
-                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
-                transition-all hover:bg-jade hover:text-black`}
-                
-                >
-                github.com/MarkosComK
-            </a>
-            <a
-                className={`
-                ${display ? "flex" : ""}
-                delay-100 items-center justify-center border h-full m-2 z-10 hover:p-10 hover:animate-pulse
-                transition-all hover:bg-jade hover:text-black`}
-                
-                >
-                linkedin
-            </a>
-        </div>
-    </div>
+    <section
+    className={`${display ? "open bg-white dark:bg-gray-900 border-t border-r border-b" : "close custom-hover border-b"} transition-all duration-500 bottom-0 bg-charcoal`} onClick={() => setContactDisplay(!display)} id="contact" >
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md relative z-50">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+              <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+              <form action="#" className="space-y-8">
+                  <div>
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                      <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                  </div>
+                  <div>
+                      <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                      <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
+                  </div>
+                  <div className="sm:col-span-2">
+                      <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                      <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                  </div>
+                  <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+              </form>
+          </div>
+          <Noise />
+    </section> 
   )
 }
