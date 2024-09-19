f9918bf64db10b9316a8d19867256e4abaede2cc
MarkosComK <markoscomks@gmail.com>
2024-09-19 18:56:14 +0100

    added font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'purple-darkest': '#2c0735', // Deep, dark purple for backgrounds or major elements
-        'purple-darker': '#4e148c', // Slightly lighter than darkest, for prominent features
-        'purple-dark': '#613dc1',   // Strong purple for headings and key accents
-        'purple-medium': '#858ae3', // Medium purple for text or secondary elements
-        'purple-light': '#97dffc',  // Light purple for backgrounds or highlights
-        
-        // Additional colors for text
-        'text-primary': '#ffffff',  // White for main text on dark backgrounds
-        'text-secondary': '#e0e0e0',// Light gray for secondary text or less prominent information
-        'text-muted': '#b0b0b0',    // Gray for less important or disabled text
-        'text-dark': '#333333',     // Very dark gray for text on light backgrounds
+        'purple-darkest': '#2c0735',
+        'purple-darker': '#4e148c',
+        'purple-dark': '#613dc1',
+        'purple-medium': '#858ae3',
+        'purple-light': '#97dffc',
+        'text-primary': '#ffffff',
+        'text-secondary': '#e0e0e0',
+        'text-muted': '#b0b0b0',
+        'text-dark': '#333333',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        'press-start': ['Press_Start_2P', 'cursive'],
       },
     },
   },
