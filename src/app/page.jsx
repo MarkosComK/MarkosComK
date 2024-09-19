25b6f8d8544fcc909df038a416dede2f196b7c1e
MarkosComK <markoscomks@gmail.com>
2024-09-19 18:53:57 +0100

    New color palette

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,86 +1,10 @@
 "use client";
-import About from "./about/page";
-import { useRef, useState } from "react";
-import Cursor from "./effects/cursor";
-import Contact from "./contact/Contact";
-import ContactButton from "./contact/ContactButton";
-import LeftCorner from "./effects/leftcorner";
-import Video from "./video/video";
-import Work from "./work/work";
-import Resume from "./resume/Resume";
 
 export default function home() {
-  const [aboutDisplay, setAboutDisplay] = useState(false);
-  const [contactDisplay, setContactDisplay] = useState(false);
-  const [workDisplay, setWorkDisplay] = useState(false);
-  const [resumeDisplay, setResumeDisplay] = useState(false);
-
-  const [index, setIndex] = useState(1000);
-  const [aboutIndex, setAboutIndex] = useState(0);
-  const [contactIndex, setContactIndex] = useState(0);
-  const [workIndex, setWorkIndex] = useState(0);
-  const [resumeIndex, setResumeIndex] = useState(0);
-
-  const [mousePos, setMousePos] = useState({x: 0, y: 0});
-
-  const handleAboutIndex = () => {
-    setAboutIndex(index);
-    setIndex(index + 1);
-  }
-
-  const handleContactIndex = () => {
-    setContactIndex(index);
-    setIndex(index + 1);
-  }
-
-  const handleWorkIndex = () => {
-    setWorkIndex(index);
-    setIndex(index + 1);
-  }
-
-  const handleResumeIndex = () => {
-    setResumeIndex(index);
-    setIndex(index + 1);
-  }
-
-  const handleMouse = (e) => {
-    setMousePos({x: e.clientX, y: e.clientY});
-  }
-
-  const contactButton = () => {
-    setAboutDisplay(!aboutDisplay);
-    setContactDisplay(!contactDisplay);
-  }
 
   return (
     <div className="overflow-hidden relative">
-      <section 
-      className="border border-jade w-svw h-svh"
-      onMouseMove={handleMouse}
-      >
-        <Cursor mousePos={mousePos} />
-        <div
-          className="grid grid-cols-2 grid-rows-4 w-full h-full lg:grid-cols-4 lg:grid-rows-2"
-        >
-          <div className="row-span-2 border lg:row-span-1 lg:col-span-2">
-            <About display={aboutDisplay} setAboutDisplay={setAboutDisplay} index={aboutIndex} handleIndex={handleAboutIndex}/>
-          </div>
-          <div className="border flex items-center justify-center">
-            <Work display={workDisplay} setWorkDisplay={setWorkDisplay} index={workIndex} handleIndex={handleWorkIndex}/>
-          </div>
-          <div className="border flex items-center justify-center">
-            <Video />
-          </div>
-          <div className="row-span-2 border w-full h-full lg:row-span-1 lg:col-span-2">
-            <Contact display={contactDisplay} setContactDisplay={setContactDisplay} index={contactIndex} handleIndex={handleContactIndex}/>
-          </div>
-          <div className="border flex items-center justify-center row-span-2 w-full h-full lg:row-span-1 col-span-2">
-            <Resume />
-          </div>
-        </div>
-        <ContactButton display={aboutDisplay} setContactDisplay={contactButton}/>
-        <LeftCorner display={aboutDisplay}/>
-      </section>
+    <p>fresh start</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-
-    },
-    colors: {
-      'black': '#000',
-      'charcoal': '#014040',
-      'green': '#02735E',
-      'jade': '#03C778',
-      'old': '#015C42',
-      'orange': '#F27405',
-      'brown': '#731702',
-    },
-    fontFamily: {
-      'sans': ['Roboto', 'sans-serif'],
-      'serif': ['Merriweather', 'serif'],
-      'custom': ['var(--custom)'],
+      colors: {
+        'purple-darkest': '#2c0735', // Deep, dark purple for backgrounds or major elements
+        'purple-darker': '#4e148c', // Slightly lighter than darkest, for prominent features
+        'purple-dark': '#613dc1',   // Strong purple for headings and key accents
+        'purple-medium': '#858ae3', // Medium purple for text or secondary elements
+        'purple-light': '#97dffc',  // Light purple for backgrounds or highlights
+        
+        // Additional colors for text
+        'text-primary': '#ffffff',  // White for main text on dark backgrounds
+        'text-secondary': '#e0e0e0',// Light gray for secondary text or less prominent information
+        'text-muted': '#b0b0b0',    // Gray for less important or disabled text
+        'text-dark': '#333333',     // Very dark gray for text on light backgrounds
+      },
     },
   },
   plugins: [],
