6e7df9a2055e045f0893377c16219f0a17421989
MarkosComK <markoscomks@gmail.com>
2024-09-19 18:46:11 +0100

    fresh start

--- src/app/effects/cursor.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cursor({ mousePos }) {
-  return (
-    <div 
-    className={`lg:inline hidden h-6 w-4 absolute z-[10000] bg-jade translate-x-3.5 animate-pulse`} 
-    style={{left: mousePos.x, top: mousePos.y}}>
-    </div>
-  )
-}
