3b299038ab05ea354f334e3f53fcbe1ffb2ff687
MarkosComK <markoscomks@gmail.com>
2025-02-09 14:55:01 +0000

    Color fixes

--- src/components/Content/styles.tsx
+++ src/components/Content/styles.tsx
@@ -15,16 +15,21 @@ export const tion = styled.section`
 	align-items: center;
 	justify-content: space-between;
 	display: grid;
-	grid-template-columns: 4fr 1fr;  /* Splits into 4/5 and 1/5 */
+	grid-template-columns: 4fr 1fr;
 	gap: 20px;
 	width: clamp(50%, 800px, 100%);
 	height: 624px;
 	padding: 0;
 	margin: 0 8px;
-		font-size: 1.5rem;
+	font-size: 1.2rem;
 	& div:first-of-type {
+		padding: 12px 16px;
+		background-color: ${({theme}) => theme.colors.mediumGreen};
 		overflow-y: auto;
 		height: 100%;
+	}
+	& div{
+		padding: 12px 16px;
 	}
 `;
 
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   scrollbar-width: thin;
-  scrollbar-color: #00ff00 ${({ theme }) => theme.colors.background};
+  scrollbar-color: ${({ theme }) => theme.colors.iron} ${({ theme }) => theme.colors.background};
 }
 
 body {
